367901bab5bfec5d0c5bc26e0fba4b1682b68da4
MattMet15 <mmetcalfe.1915@gmail.com>
2024-11-04 16:19:49 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -4,3 +4,10 @@ word_list = ['apple', 'banana', 'pear', 'mango', 'grapefruit']
 print(word_list)
 
 word = random.choice(word_list)
+
+guess = input("Please Enter a single letter guess: ")
+if len(guess) == 1:
+    print("Good guess!.")
+else:
+    print("Oops! That is not a valid input.")
+
