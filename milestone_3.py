1c44f1ec805c6c96fd2abad70daa39a95c4475c5
MattMet15 <mmetcalfe.1915@gmail.com>
2024-11-04 16:30:17 +0000

    Create milestone_3.py

---
+++ milestone_3.py
@@ -0,0 +1,15 @@
+def ask_for_input():
+  guess.lower()
+  ## Loop to chek if the input from the user is an alphabetical character - will use isalpha
+  while True:
+    guess = input("Please Guess a letter: ")
+    if guess.isalpha() == True:
+      break
+    else:
+      print("Invalid letter. Please, enter a single alphabetical character.")
+
+def check_guess(guess):
+  if guess in word:
+    print(f"Good guess!{guess} is in the word")
+  else:
+    print(f"Sorry, {guess} is not in the word. Try again!")
